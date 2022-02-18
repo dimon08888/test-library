c1cb0bacbc4e0a66e1c830081ec7af225204f4a6
dimon08888 <dimon08888@mail.ru>
2022-02-18 13:01:15 +0300

    Исправлена ширина категорий на странице детали

--- src/App.tsx
+++ src/App.tsx
@@ -181,7 +181,7 @@ function DetailPage() {
         <div className="w-56 h-80 bg-slate-200"></div>
       )}
       <div>
-        <div className="font-bold mb-4">{book.volumeInfo.categories}</div>
+        <div className="font-bold mb-4 max-w-sm">{book.volumeInfo.categories}</div>
         <div className="font-bold mb-4">{book.volumeInfo.title}</div>
         <div className="font-bold mb-4">{book.volumeInfo.authors}</div>
         <div className="max-w-xl border-2 border-solid border-black px-3 py-3 bg-stone-100">
